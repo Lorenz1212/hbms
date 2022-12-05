0b03319609a6367ec3730931986c5dc4d994dd72
Lorenz1212 <42542401+Lorenz1212@users.noreply.github.com>
2022-12-05 17:02:51 +0800

    update

--- landingpage/gallery.php
+++ landingpage/gallery.php
@@ -1,4 +1,17 @@
 <?php include_once('header.php') ?>
+<div class="breadcrumb-area bg-img bg-overlay jarallax" style="background-image: url(img/bg-img/16.jpg);">
+<div class="container h-100">
+<div class="row h-100 align-items-center">
+<div class="col-12">
+<div class="breadcrumb-content text-center">
+<h2 class="page-title">Gallery</h2>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+
 <section class="roberto-about-area section-padding-100-0">
 
 <div class="container mt-100">
